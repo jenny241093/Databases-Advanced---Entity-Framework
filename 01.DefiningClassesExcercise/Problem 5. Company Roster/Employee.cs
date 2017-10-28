c7533251931667e2110b7b1a3fee1d741a654824
Jeni <jeni241093@gmail.com>
2017-10-28 19:20:50 +0300

    AddingSomeTasks

---
+++ 01.DefiningClassesExcercise/Problem 5. Company Roster/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+  public  class Employee
+  {
+      private string name;
+      private double salary;
+    string position;
+      private string department;
+      private string email;
+      private int age;
+
+      public Employee(string name, double salary, string position, string department, string email, int age)
+      {
+          this.Name = name;
+          this.Salary = salary;
+          this.Position = position;
+          this.Department = department;
+          this.Email = email;
+          this.Age = age;
+      }
+      public Employee(string name, double salary, string position, string department)
+      {
+          this.Name = name;
+          this.Salary = salary;
+          this.Position = position;
+          this.Department = department;
+          this.Email = "n/a";
+          this.Age = -1;
+      }
+      public Employee(string name, double salary, string position, string department,string email)
+      {
+          this.Name = name;
+          this.Salary = salary;
+          this.Position = position;
+          this.Department = department;
+          this.Email =email;
+          this.Age = -1;
+      }
+      public Employee(string name, double salary, string position, string department,int age)
+      {
+          this.Name = name;
+          this.Salary = salary;
+          this.Position = position;
+          this.Department = department;
+          this.Email = "n/a";
+          this.Age = age;
+      }
+    public string Name { get => name; set => name = value; }
+    public double Salary { get => salary; set => salary = value; }
+    public string Position { get => position; set => position = value; }
+    public string Department { get => department; set => department = value; }
+    public string Email { get => email; set => email = value; }
+    public int Age { get => age; set => age = value; }
+      public override string ToString()
+      {
+          return $"{this.Name} {this.Salary:f2} {this.Email} {this.Age}";
+      }
+  }
+
---
+++ 01.DefiningClassesExcercise/Problem 5. Company Roster/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        var employees = new List<Employee>();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var name = cmdArgs[0];
+            var salary = double.Parse(cmdArgs[1]);
+            var position = cmdArgs[2];
+            var department = cmdArgs[3];
+            if (cmdArgs.Length == 6)
+            {
+                var email = cmdArgs[4];
+                var age = int.Parse(cmdArgs[5]);
+                var employee = new Employee(name, salary, position, department, email, age);
+                employees.Add(employee);
+            }
+            else if (cmdArgs.Length == 5)
+            {
+
+                var isAge = 0;
+                if (int.TryParse(cmdArgs[4], out isAge))
+                {
+                    Employee employee = new Employee(name, salary, position, department, isAge);
+                }
+                else
+                {
+                    var email = cmdArgs[4];
+                    Employee employee = new Employee(name, salary, position, department, email);
+                    employees.Add(employee);
+                }
+            }
+            else if (cmdArgs.Length==4)
+            {
+                var employee = new Employee(name, salary, position, department);
+                employees.Add(employee);
+            }
+        }
+        Console.WriteLine(Print(employees));
+    }
+    public static string Print(List<Employee> employees)
+    {
+        StringBuilder sb = new StringBuilder();
+        var highestSalaryDepartment = employees.Max(x => x.Salary);
+        var department = employees.FirstOrDefault(x => x.Salary >= highestSalaryDepartment).Department;
+        sb.AppendLine($"Highest Average Salary: {department}");
+        var departmentEmployees = employees.Where(x => x.Department == department).ToList();
+        foreach (var emp in departmentEmployees.OrderByDescending(x => x.Salary))
+        {
+            sb.AppendLine(emp.ToString());
+        }
+        return sb.ToString().Trim();
+    }
+}
+
