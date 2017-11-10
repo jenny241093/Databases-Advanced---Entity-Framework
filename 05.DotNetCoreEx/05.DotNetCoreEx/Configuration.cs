03ba754a58a02581963a26075729a607960ba18e
Jeni <jeni241093@gmail.com>
2017-11-10 11:53:06 +0200

    SuccessfullySolved.NetIssues

---
+++ 05.DotNetCoreEx/05.DotNetCoreEx/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02_DatabaseFirst
+{
+   public static class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-5CTIM8C\SQLEXPRESS;Database=SoftUni;Integrated Security=True";
+    }
+}
---
+++ 05.DotNetCoreEx/05.DotNetCoreEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using P02_DatabaseFirst.Data.Models;
+using P02_DatabaseFirst.Data;
+using System.Linq;
+
+namespace P02_DatabaseFirst
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+            var employees = dbContext.Employees.ToList();
+            foreach (var emp in employees)
+            {
+                Console.Write($"{emp.FirstName} ");
+                Console.Write($"{emp.LastName} ");
+                Console.Write($"{emp.MiddleName} ");
+                Console.Write($"{emp.JobTitle} ");
+                Console.Write($"{emp.Salary:F2}");
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
